7bb35cd7480e104c677f4e0ecef714fbfc3df8bd
Doug Durham <ddurham@dontpaniclabs.com>
2019-12-29 15:30:46 -0600

    Created the solution and project

---
+++ ProgrammingBasics/ProgrammingBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingBasics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            NumericTypes();
+        }
+
+        public static void NumericTypes()
+        {
+            // Declarations
+
+            // Assignments
+
+            // 
+        }
+    }
+}
