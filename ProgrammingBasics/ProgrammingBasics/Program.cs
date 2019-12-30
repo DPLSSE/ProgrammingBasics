442b0c82a1fd9524483c7a6ac54b7960bc209b2c
Doug Durham <ddurham@dontpaniclabs.com>
2019-12-29 18:18:32 -0600

    Selection statements

--- ProgrammingBasics/ProgrammingBasics/Program.cs
+++ ProgrammingBasics/ProgrammingBasics/Program.cs
@@ -6,21 +6,24 @@ namespace ProgrammingBasics
     {
         public static void Main()
         {
-            Variables();
 
-            Constants();
+            //Variables();
+            //Constants();
+            //DataTypes();
 
-            DataTypes();
+            //ArithmeticOperations();
+            //OrderOfOperations();
+            //ComparisonOperations();
+            //LogicalOperations();
 
-            ArithmeticOperations();
+            //StandardIO();
 
-            OrderOfOperations();
-
-            ComparisonOperations();
-
-            LogicalOperations();
-
-            StandardIO();
+            //SequenceStructure();
+            //IfStatement();
+            //IfElseStatement();
+            //IfElseIfStatement();
+            //NestedIfStatement();
+            SwitchStatement();
 
             // Wait for user to hit a key before quitting
             Console.WriteLine("Hit any key to quit");
@@ -161,6 +164,8 @@ namespace ProgrammingBasics
 
         public static void StandardIO()
         {
+            Console.WriteLine("*** STANDARD IO ***");
+
             Console.WriteLine("Press your favorite key"); // Writes the string to console
             ConsoleKeyInfo keyInfo = Console.ReadKey(); // Returns the value of the pressed key
             Console.WriteLine("You pressed " + keyInfo.Key.ToString());
@@ -168,6 +173,147 @@ namespace ProgrammingBasics
             Console.WriteLine("Type something and press return");
             string lineInfo = Console.ReadLine(); // Returns the line of text after hitting return
             Console.WriteLine("You entered " + lineInfo);
+        }
+
+        // References
+        // https://www.hunker.com/13416128/recommended-humidity-based-on-the-temperature-in-the-house
+        // https://www.pkwadsworth.com/blog/whats-the-proper-humidity-level-for-your-home-in-winter
+
+        public static void SequenceStructure()
+        {
+            Console.WriteLine("*** SEQUENCE STRUCTURE ***");
+
+            Console.WriteLine("Enter the current temperature in Fahrenheit");
+            string enteredTemp = Console.ReadLine();
+
+            int fahrenheitTemp;
+            int.TryParse(enteredTemp, out fahrenheitTemp);
+
+            int celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+            Console.WriteLine("The current temp in celsius is " + celsiusTemp);
+        }
+
+        public static void IfStatement()
+        {
+            Console.WriteLine("*** IF STATEMENT ***");
+
+            Console.WriteLine("Enter the current temperature in Fahrenheit");
+            string enteredTemp = Console.ReadLine();
+
+            int fahrenheitTemp;
+            bool isInt = int.TryParse(enteredTemp, out fahrenheitTemp);
+
+            if (isInt) // Only do the calculation if the data entered is an integer
+            {
+                int celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+                Console.WriteLine("The current temp in celsius is " + celsiusTemp);
+            }
+        }
+
+        public static void IfElseStatement()
+        {
+            Console.WriteLine("*** IF-ELSE STATEMENT ***");
+
+            Console.WriteLine("Enter the current temperature in Fahrenheit");
+            string enteredTemp = Console.ReadLine();
+
+            int fahrenheitTemp;
+            bool isInt = int.TryParse(enteredTemp, out fahrenheitTemp);
+
+            if (isInt) // Only do the calculation if the data entered is an integer
+            {
+                int celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+                Console.WriteLine("The current temp in celsius is " + celsiusTemp);
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid number");
+            }
+        }
+
+        public static void IfElseIfStatement()
+        {
+            Console.WriteLine("*** IF-ELSE-IF STATEMENT ***");
+
+            Console.WriteLine("Enter the current temperature in Fahrenheit");
+            string enteredTemp = Console.ReadLine();
+
+            int fahrenheitTemp;
+            bool isInt = int.TryParse(enteredTemp, out fahrenheitTemp);
+
+            if (fahrenheitTemp >= 20)
+            {
+                Console.WriteLine("Ideal humidity is 50%");
+            }
+            else if ((fahrenheitTemp < 20) & (fahrenheitTemp >= 10))
+            {
+                Console.WriteLine("Ideal humidity is 35%");
+            }
+            else // Temperature is less than 10 degrees
+            {
+                Console.WriteLine("Ideal humidity is 30%");
+            }
+        }
+
+        public static void NestedIfStatement()
+        {
+            Console.WriteLine("*** NESTED IF STATEMENT ***");
+
+            Console.WriteLine("Enter the current temperature in Fahrenheit");
+            string enteredTemp = Console.ReadLine();
+
+            int fahrenheitTemp;
+            bool isInt = int.TryParse(enteredTemp, out fahrenheitTemp);
+
+            if (isInt) // Only do the calculation if the data entered is an integer
+            {
+                if (fahrenheitTemp > 40)
+                {
+                    Console.WriteLine("Ideal humidity is 50%");
+                }
+                else
+                {
+                    Console.WriteLine("Ideal humidity is 35%");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid number");
+            }
+        }
+
+        public static void SwitchStatement()
+        {
+            Console.WriteLine("*** SWITCH STATEMENT ***");
+
+            Console.WriteLine("Enter the number for the current season");
+            Console.WriteLine("1 = Winter");
+            Console.WriteLine("2 = Spring");
+            Console.WriteLine("3 = Summer");
+            Console.WriteLine("4 = Fall");
+            string currentSeason = Console.ReadLine();
+
+            switch (currentSeason)
+            {
+                case "1":
+                    Console.WriteLine("Set humidity to 30% for Winter");
+                    break;
+                case "2":
+                    Console.WriteLine("Set humidity to 40% for Spring");
+                    break;
+                case "3":
+                    Console.WriteLine("Set humidity to 50% for Summer");
+                    break;
+                case "4":
+                    Console.WriteLine("Set humidity to 35% for Fall");
+                    break;
+                default:
+                    Console.WriteLine("You did not enter a valid season");
+                    break;
+            }
         }
     }
 }
